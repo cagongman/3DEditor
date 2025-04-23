5df8297d3966e4fa7218802303be35ebd5844587
Kisub Lee <cagongman@gmail.com>
2025-04-23 20:56:59 +0900

    Add Assimp library to Load OBJ

--- CM_3DEditor/CM_3DEditor/FileManager.cpp
+++ CM_3DEditor/CM_3DEditor/FileManager.cpp
@@ -1,11 +1,32 @@
 #include "FileManager.h"
 #include "Scene.h"
+#include <Qt3DRender/QMesh>
+#include <QUrl>
+
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+#include <QDebug>
 
 FileManager::FileManager(QObject* parent)
     : QObject(parent) {}
 
 bool FileManager::loadOBJ(const QString& path, Scene* scene) {
     // TODO: parse OBJ and add to scene
+
+    Assimp::Importer importer;
+    const aiScene* aiScene = importer.ReadFile(
+        path.toStdString(),
+        aiProcess_Triangulate |
+        aiProcess_GenSmoothNormals |
+        aiProcess_JoinIdenticalVertices
+    );
+
+    if (!aiScene || aiScene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || aiScene->mNumMeshes == 0) {
+        qWarning() << "Assimp OBJ load error: " << importer.GetErrorString();
+        return false;
+    }
+
     emit sceneUpdated();
     return true;
 }
